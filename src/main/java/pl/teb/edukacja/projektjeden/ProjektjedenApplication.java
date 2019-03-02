1f5c89d192afaaac673bd0312a791601c05806c2
Emilia Trojanowska <emitrojan93@gmail.com>
2019-03-02 12:48:03 +0100

    Wiosna

---
+++ src/main/java/pl/teb/edukacja/projektjeden/ProjektjedenApplication.java
@@ -0,0 +1,13 @@
+package pl.teb.edukacja.projektjeden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektjedenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektjedenApplication.class, args);
+	}
+
+}
